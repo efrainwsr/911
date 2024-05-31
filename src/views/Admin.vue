cf7e6c9b8da3471a2cf18cb841a28e3168d23f49
efrainwsr <efrain.figueredowsr@gmail.com>
2024-05-31 13:25:19 -0400

    INIT

---
+++ src/views/Admin.vue
@@ -0,0 +1,9 @@
+<script setup>
+    import AdminCrearUsuarioFrm from '../components/AdminCrearUsuarioFrm.vue'
+    import AdminVerUsuariosTable from '../components/AdminVerUsuariosTable.vue';
+</script>
+
+<template>
+    <AdminCrearUsuarioFrm/>
+    <AdminVerUsuariosTable/>
+</template>
